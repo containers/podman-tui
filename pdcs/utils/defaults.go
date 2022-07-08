7b35f80a36a3ed60c950d3ca2cd9100f22a9f350
Navid Yaghoobi <navidys@fedoraproject.org>
2022-07-09 08:35:00 +1000

    running golangci-lint on pdcs/volumes
    Signed-off-by: Navid Yaghoobi <navidys@fedoraproject.org>

--- pdcs/utils/defaults.go
+++ pdcs/utils/defaults.go
@@ -1,6 +1,11 @@
 package utils
 
+import "fmt"
+
 const (
 	// DefaultContainerDetachKeys container's default attach keys string.
 	DefaultContainerDetachKeys = "ctrl-p,ctrl-q"
 )
+
+// ErrEmptyVolDest empty volume destination error.
+var ErrEmptyVolDest = fmt.Errorf("volume destination cannot be empty")
--- pdcs/volumes/remove.go
+++ pdcs/volumes/remove.go
@@ -6,12 +6,14 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// Remove removes the volulme
+// Remove removes the volulme.
 func Remove(name string) error {
 	log.Debug().Msgf("pdcs: podman volume remove %s", name)
+
 	conn, err := registry.GetConnection()
 	if err != nil {
 		return err
 	}
+
 	return volumes.Remove(conn, name, new(volumes.RemoveOptions))
 }
